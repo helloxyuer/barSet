b6f69dcaef50bf5ca83e7038dfdd92226fba72d7
bychen <874742015@qq.com>
2018-11-19 12:56:22 +0800

    session中取openid

--- js/checkOpenId.js
+++ js/checkOpenId.js
@@ -1,7 +1,7 @@
 var appid = 'wx11a1b266d02b9112';
 var params = new UrlSearch();
 var local = window.location.href;
-if(!sessionStorage.getItem('openid')||!params.code){
+if(!sessionStorage.getItem('openid')&&!params.code){
     window.location.href ='https://open.weixin.qq.com/connect/oauth2/authorize?appid='+appid+'&redirect_uri='+encodeURIComponent(local)+'&response_type=code&scope=snsapi_base&state=1&connect_redirect=1#wechat_redirect'
 }else{
     if(!sessionStorage.getItem('openid')){
